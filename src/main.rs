d027ec2f0629afc84b2a9bb8dd08a3e415b51d7c
h-ueno2 <menbou1212@gmail.com>
2020-05-17 09:07:01 +0900

    ひとまずベースを作成

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    for i in 1..101 {
+        let answer = match i {
+            i if i % 15 == 0 => String::from("FizzBuzz"),
+            i if i % 3 == 0 => String::from("Fizz"),
+            i if i % 5 == 0 => String::from("Buzz"),
+            _ => i.to_string(),
+        };
+        println!("{}",answer);
+    }
 }
