0a2e79947fbf2b625277e8062aef3e4e3e942569
h-ueno2 <menbou1212@gmail.com>
2020-05-17 17:27:31 +0900

    モジュールに分割してみる

---
+++ src/lib.rs
@@ -0,0 +1,36 @@
+pub struct Manager {
+    fizz_num: u32,
+    buzz_num: u32,
+    fizz_buzz_num: u32,
+    max: u32,
+}
+
+impl Manager {
+    pub fn new(max: u32) -> Manager {
+        let fizz_num = 3;
+        let buzz_num = 5;
+        let fizz_buzz_num = fizz_num * buzz_num;
+        let max = max + 1;
+        Manager {
+            fizz_num,
+            buzz_num,
+            fizz_buzz_num,
+            max,
+        }
+    }
+
+    pub fn make_answer(&self, num: u32) -> String {
+        match num {
+            num if num % self.fizz_buzz_num == 0 => String::from("FizzBuzz"),
+            num if num % self.fizz_num == 0 => String::from("Fizz"),
+            num if num % self.buzz_num == 0 => String::from("Buzz"),
+            _ => num.to_string(),
+        }
+    }
+
+    pub fn run(&self) {
+        for i in 1..self.max {
+            println!("{}", self.make_answer(i));
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
+extern crate fizz_buzz;
+
+use fizz_buzz::Manager;
+
 fn main() {
-    for i in 1..101 {
-        let answer = match i {
-            i if i % 15 == 0 => String::from("FizzBuzz"),
-            i if i % 3 == 0 => String::from("Fizz"),
-            i if i % 5 == 0 => String::from("Buzz"),
-            _ => i.to_string(),
-        };
-        println!("{}",answer);
-    }
+    Manager::new(100).run();
 }
