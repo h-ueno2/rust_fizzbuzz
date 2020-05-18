ea0aadef24aee0538d65b2c5dd508be214dd1de0
h-ueno2 <menbou1212@gmail.com>
2020-05-18 10:35:33 +0900

    コメント追加

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+/// `Manager`は`FizzBuzz`ゲームを管理します。
 pub struct Manager {
     fizz_num: u32,
     buzz_num: u32,
@@ -6,6 +7,13 @@ pub struct Manager {
 }
 
 impl Manager {
+    /// 新しい`Manager`を作成します。引数に応じてルールを設定します。
+    /// 
+    /// # Argments
+    /// 
+    /// * `fizz_num` - 値を`Fizz`とするための基準値
+    /// * `buzz_num` - 値を`Buzz`とするための基準値
+    /// * `max` - FizzBuzzゲームを繰り返し行う最大値
     pub fn new(fizz_num:u32, buzz_num:u32, max: u32) -> Manager {
         let fizz_buzz_num = fizz_num * buzz_num;
         let max = max + 1;
@@ -17,6 +25,20 @@ impl Manager {
         }
     }
 
+    /// 与えられた数値から回答を作成して返却します。
+    /// 
+    /// # Arguments
+    /// 
+    /// * `num` - 対象の数値
+    /// 
+    /// # Return value
+    /// 
+    /// 返却値は`num`によって以下の値をとります。
+    /// 
+    /// * `fizz_num`で割り切れる数字であった場合は`Fizz`を返却します。
+    /// * `buzz_num`で割り切れる数字であった場合は`Buzz`を返却します。
+    /// * `fizz_num`及び`buzz_num`の両方で割り切れる値であった場合は`FizzBuzz`を返却します。
+    /// * 上記以外の値で割り切れる値であった場合は`num`を返却します。
     pub fn make_answer(&self, num: u32) -> String {
         match num {
             num if num % self.fizz_buzz_num == 0 => String::from("FizzBuzz"),
@@ -26,6 +48,7 @@ impl Manager {
         }
     }
 
+    /// 1からmaxまでの値を対象にFizzBuzzを実行します。
     pub fn run(&self) {
         for i in 1..self.max {
             println!("{}", self.make_answer(i));
