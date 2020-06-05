f0d4a7c682e7adb3ea3651cb092e4bc63f5f4b82
h-ueno2 <menbou1212@gmail.com>
2020-06-05 20:59:41 +0900

    引数で最大数を制御

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 extern crate fizz_buzz;
 
+use std::env;
 use fizz_buzz::Manager;
 
 fn main() {
-    let result = Manager::new(3, 5, 100).run();
+    let args: Vec<String> = env::args().collect();
+    let max: u32 = args[1].parse().unwrap();
+    let result = Manager::new(3, 5, max).run();
     println!("{}", result);
 }
