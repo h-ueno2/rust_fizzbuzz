3ac56f36999c5ce06ce60f195fed325d7b2b45fb
h-ueno2 <menbou1212@gmail.com>
2020-06-06 19:44:14 +0900

    引数制御

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,18 @@ use fizz_buzz::Manager;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let max: u32 = args[1].parse().unwrap();
+    let max = parse_config(&args);
     let result = Manager::new(3, 5, max).run();
     println!("{}", result);
+}
+
+fn parse_config(args: &[String]) -> u32 {
+    let default_max = 100;
+    if args.len() <= 1 {
+        return default_max
+    }
+    let max: u32 = args[1].parse().unwrap_or_else(|_| {
+        default_max
+    });
+    max
 }
